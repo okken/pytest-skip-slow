04132a528fb918f72f285d48fc61e127159584fe
Brian Okken <1568356+okken@users.noreply.github.com>
2022-04-26 06:00:16 -0700

    rename plugin.py pytest_skip_slow.py also run black, bump version, and start gh actions

--- examples/test_slow.py
+++ examples/test_slow.py
@@ -1,9 +1,10 @@
 import pytest
 
+
 def test_normal():
     pass
+
 
 @pytest.mark.slow
 def test_slow():
     pass
-
--- pytest_skip_slow.py
+++ pytest_skip_slow.py
@@ -1,19 +1,23 @@
+# START: top
 """
 A pytest plugin to skip `@pytest.mark.slow` tests by default. 
 Include the slow tests with `--slow`.
 """
 
-__version__ = '0.0.2'
-
 import pytest
+
+
+__version__ = "0.0.3"
+
 
 def pytest_configure(config):
     config.addinivalue_line("markers", "slow: mark test as slow to run")
 
+
 def pytest_addoption(parser):
     parser.addoption("--slow", action="store_true",
-                     default=False,
                      help="include tests marked slow")
+
 
 def pytest_collection_modifyitems(config, items):
     if not config.getoption("--slow"):
@@ -21,4 +25,3 @@ def pytest_collection_modifyitems(config, items):
         for item in items:
             if item.get_closest_marker("slow"):
                 item.add_marker(skip_slow)
-
